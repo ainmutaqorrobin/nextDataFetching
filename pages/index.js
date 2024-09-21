fba09dff066563da51e9d166708552606c56a578
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-09-21 11:48:13 +0800

    read data using file system

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
+import fs from "fs/promises";
+import path from "path";
 export default function HomePage(props) {
   const { products } = props;
   return (
     <ul>
-      {products.map((product) => (
+      {products?.map((product) => (
         <li key={product.id}>{product.title}</li>
       ))}
     </ul>
   );
 }
 export async function getStaticProps() {
-  return { props: { product: [{ id: "p1", title: "Product 1" }] } };
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return { props: { products: data.products } };
 }
