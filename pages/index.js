6905582885d4d6b851e07dbfa748fb9996cc0849
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-09-22 07:56:21 +0800

    some of config option

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import fs from "fs/promises";
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 import path from "path";
 export default function HomePage(props) {
   const { products } = props;
@@ -17,6 +17,10 @@ export async function getStaticProps() {
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  if (!data) return { redirect: { destination: "/no-data" } };
+  if (data.products.length === 0) return { notFound: true };
+
   return {
     props: { products: data.products },
     revalidate: 2 /*depends on application (sec)*/,
